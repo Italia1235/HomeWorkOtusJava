e90505ce72ff1f01bf788b386a67610ce1ceb53a
Italia1235 <1437dada@mail.ru>
2022-09-16 21:37:12 +0600

    Add select sort ; and comapre sorting algo

--- src/CompareSort.java
+++ src/CompareSort.java
@@ -6,20 +6,27 @@ import java.util.Random;
 public class CompareSort {
 
     public void compareSort () {
+        //155023600
         List<Integer> bubbleList =  new ArrayList<>();
-        for (int i = 0; i < 60000; i++) {
+        for (int i = 0; i < 6000; i++) {
             bubbleList.add(new Random().nextInt(500));
         }
-
+        //50244300
         List<Integer> selectSort =  new ArrayList<>();
-        for (int i = 0; i < 60000; i++) {
-            bubbleList.add(new Random().nextInt(500));
+        for (int i = 0; i < 6000; i++) {
+            selectSort.add(new Random().nextInt(500));
         }
-
+        //5541900
         List<Integer> sortList =  new ArrayList<>();
-        for (int i = 0; i < 60000; i++) {
+        for (int i = 0; i < 6000; i++) {
             sortList.add(new Random().nextInt(500));
         }
+
+        long startTime2 = System.nanoTime();
+        Collections.sort(sortList);
+        long estimatedTime2 = System.nanoTime() - startTime2;
+        System.out.println("COLLECTION SORT WORKED IN" + " "+ estimatedTime2+ " " + " nanosec");
+
 
         long startTime = System.nanoTime();
         new SortBubble().sort(bubbleList);
@@ -29,13 +36,10 @@ public class CompareSort {
 
         long startTime3 = System.nanoTime();
         new SelectSort().sort(selectSort);
-        long estimatedTime3 = System.nanoTime() - startTime;
+        long estimatedTime3 = System.nanoTime() - startTime3;
         System.out.println("Select Sort WORKED IN" + " "+ estimatedTime3+ " " + " nanosec");
 
-        long startTime2 = System.nanoTime();
-        Collections.sort(sortList);
-        long estimatedTime2 = System.nanoTime() - startTime;
-        System.out.println("COLLECTION SORT WORKED IN" + " "+ estimatedTime2+ " " + " nanosec");
+
 
     }
 }
